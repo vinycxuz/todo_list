99363c2bb818e37c386a89ef5999e7955980b476
vinycxuz <vinyby@gmail.com>
2024-11-29 00:23:01 -0300

    (0.1)(feat): created a put method to update User values using findByIDAndUpdate

--- index.js
+++ index.js
@@ -50,6 +50,39 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const updateUser = await User.findById(id);
+    res.status(200).json(updateUser);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted' });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 mongoose.connect(process.env.STRING_CONNECTION)
   .then(() => {
     console.log('Connected to MongoDB');
