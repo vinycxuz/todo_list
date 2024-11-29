cecc75681d6d825febd0b3bf88173f18df785e1f
vinycxuz <vinyby@gmail.com>
2024-11-29 00:06:55 -0300

    (0.1)(feat): created a mongodb connection for ToDo cluster

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const User = require('./models/User.model');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('First Server Test');
 });
+
+mongoose.connect(process.env.STRING_CONNECTION)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
