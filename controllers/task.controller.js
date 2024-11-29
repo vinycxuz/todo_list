2b3a69420fe10a9038e03ae243752227b68b00c0
vinycxuz <vinyby@gmail.com>
2024-11-29 02:15:22 -0300

    (0.1)(feat): increment updateTask for apply real time in updated_date ever we use updateTask

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -8,3 +8,50 @@ module.exports.createTask = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+module.exports.updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = {
+      ...req.body,
+      updated_date: Date.now()
+    }
+    const task = await Task.findByIdAndUpdate(id, updatedData, { new: true });
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    const updateTask = await Task.findById(id);
+    res.status(200).json(updateTask);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json({ message: 'Task deleted' });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,6 +1,9 @@
-const { createTask } = require('../controllers/task.controller');
+const { createTask, updateTask, deleteTask, getTasks } = require('../controllers/task.controller');
 const taskRouter = require('express').Router();
 
 taskRouter.post('/create', createTask);
+taskRouter.put('/update/:id', updateTask);
+taskRouter.delete('/delete/:id', deleteTask);
+taskRouter.get('/', getTasks);
 
 module.exports = taskRouter;
