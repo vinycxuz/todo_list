a7291ca2a274988f32b16ffea125f67704b5cb4f
vinycxuz <vinyby@gmail.com>
2024-12-01 15:14:31 -0300

    (1.0)(fix): clean routes to get users for test and finished the backend mvp

--- index.js
+++ index.js
@@ -25,51 +25,6 @@ app.get('/', (req, res) => {
 app.use('/users', userRouter);
 app.use('/tasks', taskRouter);
 
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-/*
-app.put('/users/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByIdAndUpdate(id, req.body);
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const updateUser = await User.findById(id);
-    res.status(200).json(updateUser);
-
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-app.delete('/users/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByIdAndDelete(id);
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.status(200).json({ message: 'User deleted' });
-
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-*/
-
 connectDB();
 connectRedis();
 
