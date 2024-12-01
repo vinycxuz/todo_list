21c6a497d93627cf69b6a332bb06f64866b1451f
vinycxuz <vinyby@gmail.com>
2024-12-01 18:30:00 -0300

    (1.0)(feat): created a requisiton form to do login in route /login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/users/login", formData);
+      console.log("User logged in:", response.data);
+    } catch (error) {
+      console.error("Error logging in:", error.response.data);
+    }
+  };
+
   return (
     <div>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
         </div>
         <button type="submit">Login</button>
       </form>
