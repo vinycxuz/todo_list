80c650d8c0ececb391f3b90dd174aa20926a9b33
vinycxuz <vinyby@gmail.com>
2024-12-01 18:12:25 -0300

    (1.0)(frontend): created a simple components with the routes /register, /login and /tasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import Login from './components/Login';
+import Register from './components/Register';
+import Tasks from './components/Tasks';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,21 @@
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
