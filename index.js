93bdee91281a3b9c9d630591bd1167c263a1115d
vinycxuz <vinyby@gmail.com>
2024-12-02 14:15:37 -0300

    test

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import cors from 'cors';
 
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
 import userRouter from './routes/user.routes.js';
 import taskRouter from './routes/task.routes.js';
@@ -13,8 +11,6 @@ import { connectRedis } from './database/redisClient.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,11 +18,7 @@ const port = process.env.PORT || 3000;
 app.use(cookieParser());
 app.use(json());
 
-app.use(cors({
-  origin: 'https://todo-list-vinycxuz-902a28c21ca1.herokuapp.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use('/users', userRouter);
 app.use('/tasks', taskRouter);
@@ -34,6 +26,10 @@ app.use('/tasks', taskRouter);
 async function startServer() {
   await connectDB();
   await connectRedis();
+
+  app.get('/', (req, res) => {
+    res.send('working');
+  })
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
