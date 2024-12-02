f0422d29227b7347efa05c1a973262b0eb4426c0
vinycxuz <vinyby@gmail.com>
2024-12-02 01:50:29 -0300

    fix

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ async function startServer() {
   await connectDB();
   await connectRedis();
 
-  app.use(express.static(path.join(__dirname, 'path/to/react/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'path/to/react/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
   });
 
   app.listen(port, () => {
