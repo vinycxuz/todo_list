5b1fe2318f7da1b2d6616cdcc73a1e440a0fa717
vinycxuz <vinyby@gmail.com>
2024-12-02 01:26:59 -0300

    test

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import cors from 'cors';
 
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
 import userRouter from './routes/user.routes.js';
 import taskRouter from './routes/task.routes.js';
@@ -13,20 +11,17 @@ import { connectRedis } from './database/redisClient.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(json());
 
-app.use(express.static(path.join(__dirname, './frontend/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/dist', 'index.html'));
-});
+app.use(cors({
+  origin: 'https://todo-list-vinycxuz-902a28c21ca1.herokuapp.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use('/users', userRouter);
 app.use('/tasks', taskRouter);
