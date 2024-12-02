1aad5aaaf5fa9e945867a1eb3f26170b6ca175cf
vinycxuz <vinyby@gmail.com>
2024-12-02 01:38:21 -0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,10 @@ app.use(cors({
 }));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, 'path/to/react/dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'path/to/react/dist', 'index.html'));
 });
 
 app.use('/users', userRouter);
