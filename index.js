5c3ab14deb18a82d7fa583eefffdf78e3f4dc7ab
vinycxuz <vinyby@gmail.com>
2024-12-02 01:12:36 -0300

    fix

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './frontend/dist', 'index.html'));
 });
 
 app.use('/users', userRouter);
